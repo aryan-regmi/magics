118571bf5bea1d285295ca247c7fe7f324750bdd
Aryan Regmi <arre9260@protonmail.com>
2023-04-13 05:23:52 -0600

    Updated test

--- tests/test_ecs.rs
+++ tests/test_ecs.rs
@@ -10,21 +10,24 @@ struct Name(&'static str);
 impl Component for Name {}
 
 fn system1(mut ctx: Context) {
-    println!("Hello from System1");
-
+    println!("[System 1] Spawning Entity 0");
     let _entity0 = ctx.spawn(
         EntityBuilder::new()
-            .with(Health(100))
             .with(Age(22))
+            .with(Health(100))
             .with(Name("Entity 0")),
     );
+    println!("{}: Entity 0 spawned", _entity0);
+
+    println!("[System 1] Spawning Entity 2");
     let _entity2 = ctx.spawn(EntityBuilder::new().with(Age(18)).with(Name("Entity 2")));
+    println!("{}: Entity 2 spawned", _entity2);
 }
 
 fn system2(mut ctx: Context) {
-    println!("Hello from System2");
-
+    println!("[System 2] Spawning Entity 1");
     let _entity1 = ctx.spawn(EntityBuilder::new().with(Health(50)).with(Name("Entity 1")));
+    println!("{}: Entity 1 spawned", _entity1);
 }
 
 #[test]
